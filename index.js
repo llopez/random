d2ca2699f700a9fdab668a1e635cc2ccebd410b8
Luis Lopez <luigibyte@gmail.com>
2016-06-10 23:17:36 -0300

    port adjustments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 3000));
+
 app.get('/', function (req, res) {
   res.send('Welcome to random!!!');
 });
@@ -11,6 +13,6 @@ app.get('/api/sample', function (req, res) {
   res.json({winner: items[i], index: i});
 });
 
-app.listen(3000, function () {
+app.listen(app.get('port'), function () {
   console.log('Example app listening on port 3000!');
 });
