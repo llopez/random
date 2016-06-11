7e7fd445ed056c5e7371bf4b617dc90b4c7dd585
Luis Lopez <luigibyte@gmail.com>
2016-06-10 23:32:34 -0300

    added format to sample url

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
   res.send('Welcome to random!!!');
 });
 
-app.get('/api/sample', function (req, res) {
+app.get('/api/sample.json', function (req, res) {
   var items = req.query.items.split(',');
   var i = parseInt(Math.random() * items.length);  
   res.json({winner: items[i], index: i});
